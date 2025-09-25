75d2fe972841d5b8ca8272aa320bd73f5a36ae00
Nanditha586 <127416252+Nanditha586@users.noreply.github.com>
2025-09-25 20:54:08 +0530

    Update settings.py

--- blogsiteproject/settings.py
+++ blogsiteproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-wg&w14ht9=j(9#wl8iul+5xsn-1*=%%nzepj)sh)4!hil_j_qo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+
+# Directory where collectstatic will collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
