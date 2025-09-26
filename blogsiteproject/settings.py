7deff3a7d351f1aa8648806fc9a84629479e175e
Nanditha586 <127416252+Nanditha586@users.noreply.github.com>
2025-09-26 13:36:06 +0530

    Update settings.py

--- blogsiteproject/settings.py
+++ blogsiteproject/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = "blogsiteproject.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
